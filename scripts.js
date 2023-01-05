4191478ccd3ad93c2ca915413251183b856ac125
Theo <sagniez.theo@laposte.net>
2023-01-05 09:45:33 +0100

    kept working on random draw logic

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const userInput  = document.querySelector('.userInput')
 const add = document.querySelector('.add')
 const ok = document.querySelector('.ok')
 const names = []
+const hasBeenPicked = []
+const pairs = []
 
 add.addEventListener('click', () => {
     if (userInput.value !== '') {
@@ -11,11 +13,23 @@ add.addEventListener('click', () => {
     }
 })
 
+
+// Needs work -> one name doesn't get picked
 ok.addEventListener('click', () => {
     names.forEach(name => {
         const namesCopy = names.slice()
         namesCopy.splice(names.indexOf(name), 1)
 
-        console.log(namesCopy)
+        hasBeenPicked.forEach(picked => {
+            if (namesCopy.includes(picked)) {
+                namesCopy.splice(namesCopy.indexOf(picked), 1)
+            }
+        })
+
+        const randomIndex = Math.floor(Math.random() * namesCopy.length)
+
+        pairs.push({name1: name, name2: namesCopy[randomIndex]})
     })
+
+    console.log(pairs)
 })
